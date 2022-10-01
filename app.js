070201e7c07a75c7c003f299ed41a8153b189c83
jesse <tomilayojesse@gmail.com>
2022-10-01 02:31:50 +0100

    first vue commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            firstName: 'Jesse',
+            lastName: 'Tomilayo',
+            email: 'Tomilayojesse@gmail.com',
+            picture: 'https://randomuser.me/api/portraits/men/75.jpg'
+        }
+    },
+    method: {
+        
+    }
+
+})
+
+app.mount('#app')
